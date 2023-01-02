501fa89d4a39156ac615fcf08d673156f854f681
BhavinSonagara <81000109+BhavinSonagara@users.noreply.github.com>
2023-01-02 23:54:57 +0530

    Create 368_Largest_Divisible_Subset.cpp

---
+++ 368_Largest_Divisible_Subset.cpp
@@ -0,0 +1,43 @@
+class Solution {
+
+public:
+
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+
+        sort(nums.begin(), nums.end());
+
+        vector<int> dp[nums.size()];
+
+        int idx=0;
+
+        for(int i = 0; i < nums.size(); i++){
+
+            for(int j = 0; j <= i; j++){
+
+                if(nums[i]%nums[j] == 0) {
+
+                    if(dp[i].size() <= dp[j].size())
+
+                    {
+
+                        dp[i] = dp[j];
+
+                    }
+
+                }
+
+            }
+
+            dp[i].push_back(nums[i]);
+
+            if(dp[i].size() >= dp[idx].size()) idx = i;
+
+        }     
+
+        return dp[idx];
+
+    }
+
+};
+        
+    
